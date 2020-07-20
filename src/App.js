322ada4e85072147ac097096c87fb775589da63f
rokpopov <rokpopov90@gmail.com>
2020-07-20 09:16:17 +0200

    Add h1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          People of the world, welcome!
-        </p>
+        <p>People of the world, welcome!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
